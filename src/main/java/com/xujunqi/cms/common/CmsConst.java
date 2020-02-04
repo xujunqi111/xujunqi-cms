a3b2c1a9152cfc7da5c77965152200ca1b1ffec8
Dell <Dell@DESKTOP-K8BFU9N>
2020-02-04 09:44:16 +0800

    再次提交CmsConst

--- src/main/java/com/xujunqi/cms/common/CmsConst.java
+++ src/main/java/com/xujunqi/cms/common/CmsConst.java
@@ -1,7 +1,7 @@
 package com.xujunqi.cms.common;
 
 public class CmsConst {
-	// :cms常量类
+	// :cms常量类jjj
 	public static String UserSessionKey = "userInfo";
 	public static String UserAdminSessionKey = "userAdminInfo";
 }
